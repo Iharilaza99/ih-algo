72e71320b01c91d61e3e2bbcd4ee4ca99564f406
Iharilaza99 <iharilazaa@gmail.com>
2025-09-07 14:48:03 +0300

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,30 @@
+function toggleMenu() { const menu = document.getElementById("menu"); menu.classList.toggle("show"); }
+const sections = {
+recu: 
+      `<div class="container">
+            <h2>Controle recu</h2>
+            <input placeholder="L'identifiant de votre recu" required type="number" id="pretInput">
+            <input placeholder="Votre code de recu" required type="number" id="pretInput">
+      </div>`,
+info: "<h2>Informatique</h2><p>Programmation, web, algorithmique.</p>",
+eco: 
+      '<div class="container">'+
+      "<h2>Économie</h2><p>Notions de base, micro/macroéconomie.</p>"+
+      "<h1>Plan d'ammortismemt a taux fixe avec remboursement constant</h1>"+
+      '<h1>Details du credit</h1>'+
+      '<input placeholder="Montant du pret en ariary" required type="number" id="pretInput" value="5000000">'+
+      '<input placeholder='+'"'+"Taux d'interet annuel en %"+'"'+'required type="number" id="taux_interet" value="15">'+
+      '<input placeholder='+'"'+"Taux d'assurance en %"+'"'+' required type="number" id="taux_assurance" value="0.80">'+
+      '<input placeholder="Periodicite durant remboursement" required type="text" id="" value="Mensuel">'+
+      '<input placeholder="Date du prett" required type="date" id="date_pret" value="05-05-2025"/>'+
+      '<input placeholder="Montant du remboursement en ariary" required type="text" id="rembInput" value="140988">'+
+      '<button class="btn" onclick="amortissememt()">Afficher</button>'+
+      '</div>',
+gestion: 
+      "<h2>Gestion</h2><p>Management, finances et comptabilité.</p>"+
+      '<input type="text" name="username" placeholder="Nom d utilisateur" required>',
+biblio: "<h2>Bibliothèque</h2><p>Ressources PDF, livres numériques.</p>",
+loisirs: "<h2>Loisirs</h2><p>Culture, jeux, musique, détente.</p>"
+};
+
+function showContent(section) { document.getElementById("content").innerHTML = sections[section]; }
