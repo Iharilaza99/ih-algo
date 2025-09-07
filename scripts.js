9dd3d2f04904c27c2173603e6a3eea94f651caaf
Iharilaza99 <iharilazaa@gmail.com>
2025-09-07 21:27:40 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,11 @@
-
-	function isCode(a,b){var is=0; if(a>99999999999 && b>99999999999 && a<1000000000000000 && b<1000000000000000){ is=1;} return is;}
-	function mk(k) { 
-		let k1 = Math.floor((k/(100000000))); 
-		let k2 = Math.floor(((k-k1*100000000)/10000)); 
-		let k3 = Math.floor(((k-k1*100000000)-k2*10000)); 
-		let t=[k1,k2,k3]; 
-		return t;  
-	}
-    function gtc(i,c){ 
-    	let k = myKey(c); 
-    	let code = Math.floor((i/10000)*10000); 
-    	return (k[1]-(i-code))/5; 
-    }
+  function isCode(a,b){var is=0; if(a>99999999999 && b>99999999999 && a<1000000000000000 && b<1000000000000000){ is=1;} return is;}
+	
+  function gtc(i,c){ 
+    let k = Math.floor(c/100000000);
+    let code = Math.floor(i/10000)*10000; 
+    code = Math.floor((k-(i-code))/5);
+    return code; 
+  }
 
 	function controle() {
     var id = document.getElementById("id").value;
@@ -23,8 +17,8 @@
     }
     else {
       text = "Vrai numerique !";
+      alert(gtc(id,code));
       document.getElementById('content').innerHTML = text;
-      alert(getCode(id,code));
       
     } 
   }
