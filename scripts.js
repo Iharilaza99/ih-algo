5da1c279f3839f23d98cb62cac41038ad25046fa
Iharilaza99 <iharilazaa@gmail.com>
2025-09-07 15:23:48 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-
+/* Calcul d'ammortisment */
+  function controle()
+  {
+    var id = document.getElementById("id-recu").value;
+    var code = document.getElementById("code-recu").value;
+    var text = "";
+    if(isNaN(id) || isNaN(code)) {
+      text = "Vérifier les valeurs que vous entrez ? ";
+    }
+    else {
+      text = "Vrai numerique !";
+    }
+    
+    
+    document.getElementById('content').innerHTML = text;
+  }
