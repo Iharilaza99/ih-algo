1cfa4f935a2171c056b9b9b89cb184424578018b
Iharilaza99 <iharilazaa@gmail.com>
2025-10-05 10:24:03 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,71 @@
-  function isCode(a,b){var is=0; if(a>99999999999 && b>99999999999 && a<1000000000000000 && b<1000000000000000){ is=1;} return is;}
-	
-  function gtc(i,c){ 
-    let k = Math.floor(c/100000000);
-    let code = Math.floor(i/10000)*10000; 
-    code = Math.floor((k-(i-code))/5);
-    return code; 
-  }
 
-	function controle() {
+function isCode(a,b){var is=0; if(a>99999999999 && b>99999999999 && a<1000000000000000 && b<1000000000000000){ is=1;} return is;}
+
+function controle() {
     var id = document.getElementById("id").value;
     var code = document.getElementById("code").value;
+
+    var societes ='LEG Andilamena';
+    var date = "27/06/2025";
+
     var text = "";
+
     if(isNaN(id) || isNaN(code) || !isCode(id, code)) { 
-      alert("Vérifier les valeurs que vous entrez ?");
-      document.getElementById("content").innerHTML = sections[recu];
+    	alert("Vérifier les valeurs que vous entrez ?");
+    	document.getElementById("content").innerHTML = sections[recu];
     }
     else {
-      text = "Vrai numerique !";
-      alert(gtc(id,code));
-      document.getElementById('content').innerHTML = text;
-      
+      number = afficheCode(getCode(code,id));
+      if(){
+        text = `
+        <div class="container">
+          <h3>INFORMATION DE VOTRE RECU</h3>
+          <p>Entreprise `+societes+`</p>
+          <p>Digital ID `+code+`</p>
+          <p>Numéro `+number+`</p>
+          <p>Date de fabrication `+date+`</p><br><br>
+        </div>`;
+      }
+      else {
+        text = " VOTRE RECU EST INNCONNU !";
+      }
+    	document.getElementById('content').innerHTML = text; 
     } 
-  }
+}
+function myKey(key) {
+  const key1 = Math.floor(key / 100000000);
+  const key2 = Math.floor((key - key1 * 100000000) / 10000);
+  const key3 = Math.floor((key - key1 * 100000000) - key2 * 10000);
+  return {
+    '1': key1,
+    '2': key2,
+    '3': key3
+  };
+}
+function getExiste(nombre){
+	const val = Math.floor(number);
+	const val1 = number - val;
+  	if(val1 == 0) { return true; }
+  	else { return false; }
+}
+function getChiffre(code, key) {
+  const k = myKey(key);
+  const n0 = k['1'] - code * 5;
+  const n1 = code * 4 + k['2'];
+  const n2 = code * 3 + k['3'];
+  return `${n2}${n1}${n0}`;
+}
+function afficheCode(nombre) {
+  return String(nombre).padStart(4, '0');
+}
+function getCode(id, key) {
+  const k = myKey(key);
+  let code = Math.floor(id / 10000) * 10000;
+  code = (k['1'] - (id - code)) / 5;
+  return code;
+}
+
+
+
+
+  
